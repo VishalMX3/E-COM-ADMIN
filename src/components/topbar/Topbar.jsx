0eaa8bbbcd76eb7cf026e709cf25998694909af8
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-19 11:27:58 +0530

    updated topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar() {
     <div className="topbar">
       <div className="topbarWrapper">
         <div className="topLeft">
-          <span className="logo">vish.admin</span>
+          <span className="logo">E-Shop.admin</span>
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
